ebdcac6386aac1a67ebb9c664dc9515cb81e226a
Shahriar Ahmed Shovon <shovonshahriar01@gmail.com>
2021-10-21 14:15:01 +0600

    Removed Redux Devtools

--- src/Components/redux/umcStore.js
+++ src/Components/redux/umcStore.js
@@ -1,22 +1,22 @@
-// Package Import
-import { createStore } from "redux"
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-// Component Import
-import UMCReducer from "./umcReducer"
-
-const store = createStore(UMCReducer, composeWithDevTools())
-
-export default store
-
-
 // // Package Import
 // import { createStore } from "redux"
-// // import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
 // // Component Import
 // import UMCReducer from "./umcReducer"
 
-// const store = createStore(UMCReducer)
+// const store = createStore(UMCReducer, composeWithDevTools())
 
 // export default store
+
+
+// Package Import
+import { createStore } from "redux"
+// import { composeWithDevTools } from 'redux-devtools-extension';
+
+// Component Import
+import UMCReducer from "./umcReducer"
+
+const store = createStore(UMCReducer)
+
+export default store
